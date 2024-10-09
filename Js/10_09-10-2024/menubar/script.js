697c6f135f1e40791e0f0e8b1b90a0a18aa122a5
urvashi <urvashi.d.bhavsar@gmail.com>
2024-10-09 11:31:07 +0530

    9th changes

---
+++ Js/10_09-10-2024/menubar/script.js
@@ -0,0 +1,9 @@
+// const showMenu = (cls) => {
+//     let myclass = document.getElementById(cls)
+//     myclass.classList.toggle("nav-show")
+// }
+
+const showMenu = (cls) => {
+    let myclass = document.getElementById(cls)
+    myclass.classList.toggle("navslideShow")
+}
