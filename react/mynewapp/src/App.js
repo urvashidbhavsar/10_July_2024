9f1d4c2716509c57369e2cde281a62b8b8517c1c
urvashi <urvashi.d.bhavsar@gmail.com>
2024-12-04 11:02:50 +0530

    4th changes

---
+++ react/mynewapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// import Msg from './Msg';
+
+function App() {
+  return (
+    <div>
+      {/* any function return in div section */}
+      {/* <Msg /> */}
+      <h1>Hello world</h1>
+      <p>this is a paragraph </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/mynewapp/src/Msg.js
@@ -0,0 +1,9 @@
+function Msg() {
+    return (
+        <div>
+            <h1>Called H1 Section</h1>
+        </div>
+    );
+}
+
+export default Msg;
